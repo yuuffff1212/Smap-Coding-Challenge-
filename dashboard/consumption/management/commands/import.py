54d62165a0cde351e215eb8eee654f83a12bcf52
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2022-09-25 14:51:30 +0900

    update import.py

--- dashboard/consumption/management/commands/import.py
+++ dashboard/consumption/management/commands/import.py
@@ -1,13 +1,13 @@
-# standard library
-from django.core.management.base import BaseCommand
-from django.conf import settings
 import datetime
 import os
-# third party library
+import logging
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
 from django.utils.timezone import make_aware
 import numpy as np
 import pandas as pd
-# Application/library specific library
+
 from consumption.models import User, Consumption, Area, Tariff
 
 
@@ -15,35 +15,32 @@ class Command(BaseCommand):
     help = 'import data'
 
     def handle(self, *args, **options):
-        user_data_path = os.path.join(os.path.dirname(settings.BASE_DIR), 'data', "user_data.csv")
-        consumption_data_path = os.path.join(os.path.dirname(settings.BASE_DIR), "data", "consumption/")
-        self.import_user_data(user_data_path)
-        self.import_consumption_data(consumption_data_path)
+        self.import_user_data(os.path.join(os.path.dirname(settings.BASE_DIR), 'data', "user_data.csv"))
+        self.import_consumption_data(os.path.join(os.path.dirname(settings.BASE_DIR), "data", "consumption/"))
 
     def import_user_data(self, user_data_path):
         """Import user data into the DB"""
-        areas_obj = {}
-        tariffs_obj = {}
 
         # Get nameObject in Area model
         areas = Area.objects.all()
-        for area in areas:
-            areas_obj = area.name
+        areas_val = areas.values()
         # Get nameObject in Tariff Model
         tariffs = Tariff.objects.all()
-        for tariff in tariffs:
-            tariffs_obj = tariff.name
+        tariffs_val = tariffs.values()
 
         try:
             user_data_df = pd.read_csv(user_data_path, chunksize=100,
                                        dtype={"id": np.int64, "area": np.object, "tariff": np.object})
             for row in user_data_df:
-                user_table = User(id=row["id"], area=areas_obj, tariff=tariffs_obj)
+                if row["area"] in areas_val:
+                    area[row["area"]] = Area.objects.get(pk=user_areas.id)
+                if row["tariff"] in tariffs_val:
+                    tariff[row["tariff"]] = Tariff.objects.get(pk=user_tariffs.id)
+                user_table = User(id=row["id"], area=area[row["area"]], tariff=tariff[row["tariff"]])
                 User.objects.bulk_create(user_table)
-                self.logger.info("Completely user data imported")
-            raise ImportError
-        except ImportError:
-            self.logger.error("User data already imported")
+                self.logging.info("Completely user data imported")
+        except Exception:
+            self.logging.error("User data already imported")
 
     def import_consumption_data(self, consumption_data_path):
         """Import Consumption data into the DB"""
@@ -55,7 +52,6 @@ class Command(BaseCommand):
                 aware_time = make_aware(datetime.datetime.strptime(row["datetime"], "%Y-%m-%d %H:%M:%S"))
                 consumption_data_table = Consumption(user=users.id, datetime=aware_time, consumption=row["consumption"])
                 Consumption.objects.bulk_create(consumption_data_table)
-                self.logger.info("Completely Consumption data imported")
-            raise ConsumptionDataImportError
-        except ConsumptionDataImportError:
-            self.logger.error("Consumption data already imported")
+                self.logging.error("Completely Consumption data imported")
+        except Exception:
+            self.logging.error("Consumption data already imported")
