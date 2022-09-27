4fa263475cb7bb47db2a129b0773c6eda71eaa89
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2022-09-27 18:03:33 +0900

    update import.py

--- dashboard/consumption/management/commands/import.py
+++ dashboard/consumption/management/commands/import.py
@@ -1,6 +1,5 @@
-import datetime
+from datetime import datetime as dt
 import os
-import logging
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
@@ -15,47 +14,40 @@ class Command(BaseCommand):
     help = 'import data'
 
     def __init__(self):
-        logging.basicConfig(level=logging.DEBUG)
-        self.logger = logging.getLogger(__name__)
+        self.user_data_path = os.path.join(os.path.dirname(settings.BASE_DIR), 'data', "user_data.csv")
+        self.consumption_data_path = os.path.join(os.path.dirname(settings.BASE_DIR), "data", "consumption/")
 
     def handle(self, *args, **options):
-        self.import_user_data(os.path.join(os.path.dirname(settings.BASE_DIR), 'data', "user_data.csv"))
-        self.import_consumption_data(os.path.join(os.path.dirname(settings.BASE_DIR), "data", "consumption/"))
+        self.import_user_data()
+        self.import_consumption_data()
 
-    def import_user_data(self, user_data_path):
+    def import_user_data(self):
         """Import user data into the DB"""
+        user_data_df = pd.read_csv(self.user_data_path, chunksize=100,
+                                   dtype={"id": np.int64, "area": np.object, "tariff": np.object})
+        for row in user_data_df:
+            area_list = Area.objects.filter(name=row["area"])
+            tariff_list = Tariff.objects.filter(name=row["tariff"])
+            if len(area_list) == 0 or len(tariff_list) == 0:
+                continue
+            area = area_list.pop()
+            tariff = tariff_list.pop()
+            User.objects.create(id=row["id"].item(), area_id=area.id, tariff=tariff.id)
 
-        # Get nameObject in Area model
-        areas = Area.objects.all()
-        areas_val = areas.values()
-        # Get nameObject in Tariff Model
-        tariffs = Tariff.objects.all()
-        tariffs_val = tariffs.values()
-
-        try:
-            user_data_df = pd.read_csv(user_data_path, chunksize=100,
-                                       dtype={"id": np.int64, "area": np.object, "tariff": np.object})
-            for row in user_data_df:
-                if row["area"] in areas_val["name"]:
-                    [row["area"]] = Area.objects.get(pk=area_id)
-                if row["tariff"] in tariffs_val:
-                    tariff[row["tariff"]] = Tariff.objects.get(pk=user_tariffs.id)
-                user_table = User(id=row["id"], area=[row["area"]], tariff=tariff[row["tariff"]])
-                User.objects.bulk_create(user_table)
-                self.logger.info("Completely user data imported")
-        except Exception:
-            self.logger.error("User data already imported")
-
-    def import_consumption_data(self, consumption_data_path):
+    def import_consumption_data(self):
         """Import Consumption data into the DB"""
-        try:
-            users = User.objects.all()
-            consumption_data_df = pd.read_csv(consumption_data_path, chunksize=100,
-                                              dtype={"datetime": np.datetime64, "consumption": np.float64})
-            for row in consumption_data_df:
-                aware_time = make_aware(datetime.datetime.strptime(row["datetime"], "%Y-%m-%d %H:%M:%S"))
-                consumption_data_table = Consumption(user=users.id, datetime=aware_time, consumption=row["consumption"])
-                Consumption.objects.bulk_create(consumption_data_table)
-                self.logger.info("Completely Consumption data imported")
-        except Exception:
-            self.logger.error("Consumption data already imported")
+        filelist = []
+        files = os.listdir(self.consumption_data_path)
+        consumption_data_table = {}
+        for filename in files:
+            if os.path.isfile(os.path.join(self.consumption_data_path, filename)):
+                consumption_data_df = pd.read_csv(files, filename, chunksize=1000, header=0, engine="python",
+                                                  dtype={"datetime": np.datetime64, "consumption": np.float64})
+                consumption_data_df["user_id"] = int(os.path.splitext(filename)[0])
+                filelist.append(consumption_data_df[["user_id", "datetime", "consumption"]])
+                consumption_data = pd.concat(filelist)
+
+                for row in consumption_data.itertuples():
+                    aware_time = make_aware(dt.strptime(row["datetime"], "%Y-%m-%d %H:%M:%S"))
+                    consumption_data_table = Consumption(user=row["user_id"], datetime=aware_time, consumption=["consumption"])
+                    Consumption.objects.create(consumption_data_table)
